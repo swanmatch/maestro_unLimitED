0183ed4e35720f415d7495acf10490a3e6f3e869
swanmatch <swan_match@yahoo.co.jp>
2017-11-04 09:46:27 +0900

    led controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,37 +10,4 @@ class ApplicationController < ActionController::Base
       }.present?
   end
 
-  def unicorn_play
-    unicorn_stop_action
-    unicorn_thread =
-      Thread.new do
-        LED.play2
-        #while(true) do end
-      end
-    unicorn_thread[:name] = 'unicorn_hat'
-    sleep 1
-    unicorn_play?
-    redirect_to root_path
-  end
-
-  def unicorn_stop
-    unicorn_stop_action
-    unicorn_play?
-    redirect_to root_path
-  end
-
-  def unicorn_stop_action
-    Thread.list.find_all{|th|
-      th[:name] == 'unicorn_hat'
-    }.each{|th|
-      th.kill
-    }
-  end
-
-  def flash
-    Thread.new do
-      LED.flash
-    end
-    render nothing: true
-  end
 end
--- app/controllers/led_controller.rb
+++ app/controllers/led_controller.rb
@@ -1,10 +1,32 @@
 class LedController < ApplicationController
-  def play
+
+  def unicorn_play
+    unicorn_stop_action
+    unicorn_thread =
+      Thread.new do
+        LED.play2
+        #while(true) do end
+      end
+    unicorn_thread[:name] = 'unicorn_hat'
+    sleep 1
+    unicorn_play?
+    redirect_to root_path
   end
 
-  def stop
+  def unicorn_stop
+    Thread.list.find_all{|th|
+      th[:name] == 'unicorn_hat'
+    }.each{|th|
+      th.kill
+    }
+    unicorn_play?
+    redirect_to root_path
   end
 
   def flash
+    Thread.new do
+      LED.flash
+    end
+    render nothing: true
   end
 end
