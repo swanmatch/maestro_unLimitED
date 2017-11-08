bab8e471852bef520937a8fde9e58cf1e77bea44
swanmatch <swan_match@yahoo.co.jp>
2017-11-09 03:22:37 +0900

    gradation

--- lib/led.rb
+++ lib/led.rb
@@ -82,7 +82,7 @@ module LED
     diffs ||= [-8, -16, -32]
 
     indexes.each do |i|
-      puts colors[i]
+      puts colors[i].inspect
       HAT[i] = Ws2812::Color.new(*colors[i])
     end
     HAT.show
