198ae4cf8a298e8c7ffc79f36aa903a7e9db2098
swanmatch <swan_match@yahoo.co.jp>
2017-11-09 03:48:54 +0900

    gradation

--- lib/led.rb
+++ lib/led.rb
@@ -101,7 +101,7 @@ module LED
       end
     sleep time
     colors[1..(size-1)].unshift(new_color)
-    puts colors
+    puts colors.inspect
     LED.gradation(indexes, colors, time, next_diffs)
   end
 
