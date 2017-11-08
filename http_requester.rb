746a887e9594dcaf04a78e1e7c4dbe6cbc5a6c10
swanmatch <swan_match@yahoo.co.jp>
2017-11-09 00:56:38 +0900

    httpRequester

--- http_requester.rb
+++ http_requester.rb
@@ -1,27 +1,29 @@
-module LED
-  def self.play2
-    require 'micromidi'
-    input = UniMIDI::Input.first
-    begin
-      MIDI.using(input) do
-        receive :note do |message|
-          if 24 < message.velocity && message.note == 36
-            #puts message.note
-            `curl -s http://192.168.43.160:3000/flash`
-          end
-        end
-        join
-      end
-    rescue Interrupt
-    # ignored
-    end
-  end
+require 'micromidi'
+input = UniMIDI::Input.first
+ip = "192.168.43.160"
 
-  def self.flash(color = nil)
-    HAT[12..30] = color || COLORS.sample
-    HAT.show
-    sleep 0.2
-    HAT[12..30] = BLACK
-    HAT.show
+puts "when stop ctr + c;"
+
+begin
+  MIDI.using(input) do
+    receive :note do |message|
+      if 36 < message.velocity
+        case message.note
+        when 36
+          # kickhttp://192.168.0.66:3000/led/flash?indexes=2,4&velocity=127&r=255&g=255&b=255
+          `curl -s http://#{ip}:3000/led/flash?velociity=#{message.velocity}&indexes=12,14,16,18,20,22,24,26,28,30`
+        when 40
+          # snare
+          `curl -s http://#{ip}:3000/led/flash?velociity=#{message.velocity}&indexes=13,15,17,19,21,23,25,27,29`
+        when 57, 49
+          # clash
+          `curl -s http://#{ip}:3000/led/flash?velociity=#{message.velocity}&r=255&g=255&b=255&time=0.07`
+        end
+        #puts message.note
+      end
+    end
+    join
   end
+rescue Interrupt
+# ignored
 end
