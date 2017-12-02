809d621bbaff6a15556b67a7c4bbd3d356265f47
swanmatch <swan_match@yahoo.co.jp>
2017-12-02 21:31:27 +0900

    Update http_requester.rb

--- http_requester.rb
+++ http_requester.rb
@@ -7,20 +7,26 @@ puts "when stop ctr + c;"
 begin
   MIDI.using(input) do
     receive :note do |message|
-      if 36 < message.velocity
+      #if 36 < message.velocity
+        puts message.note
         case message.note
         when 36
-          # kickhttp://192.168.0.66:3000/led/flash?indexes=2,4&velocity=127&r=255&g=255&b=255
-          `curl -s http://#{ip}:3000/led/flash?velociity=#{message.velocity}&indexes=12,14,16,18,20,22,24,26,28,30`
+          # kick
+          Thread.new do
+            `curl -s http://#{ip}:3000/led/flash?velociity=#{message.velocity}\\&indexes=12,14,16,18,20,22,24,26,28,30\\&tn=kick`
+          end
         when 40
           # snare
-          `curl -s http://#{ip}:3000/led/flash?velociity=#{message.velocity}&indexes=13,15,17,19,21,23,25,27,29`
+          Thread.new do
+            `curl -s http://#{ip}:3000/led/flash?velociity=#{message.velocity}\\&indexes=13,15,17,19,21,23,25,27,29\\&tn=snare`
+          end
         when 57, 49
           # clash
-          `curl -s http://#{ip}:3000/led/flash?velociity=#{message.velocity}&r=255&g=255&b=255&time=0.07`
+          Thread.new do
+            `curl -s http://#{ip}:3000/led/flash?velociity=#{message.velocity}\\&r=255\\&g=255\\&b=255\\&time=0.07\\&tn=kick,snare`
+          end
         end
-        #puts message.note
-      end
+      #end
     end
     join
   end
