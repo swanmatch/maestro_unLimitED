ab85126a9ac9bdeffca69790af966f9643187d72
swanmatch <swan_match@yahoo.co.jp>
2017-12-04 02:39:16 +0900

    auto start

--- config/initializers/midi_listener.rb
+++ config/initializers/midi_listener.rb
@@ -2,4 +2,8 @@
 # To change this template file, choose Tools | Templates
 # and open the template in the editor.
 
-LED.play
+unicorn_thread =
+  Thread.new do
+    LED.play
+  end
+unicorn_thread[:name] = 'MIDIListener'
