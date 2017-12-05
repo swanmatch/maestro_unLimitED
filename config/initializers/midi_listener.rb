8626f920eefb37ca28b60a166ad20acfadccf5cd
swanmatch <swan_match@yahoo.co.jp>
2017-12-05 15:38:16 +0900

    autostart

--- config/initializers/midi_listener.rb
+++ config/initializers/midi_listener.rb
@@ -1,9 +1,12 @@
 # To change this license header, choose License Headers in Project Properties.
 # To change this template file, choose Tools | Templates
 # and open the template in the editor.
-
-unicorn_thread =
-  Thread.new do
-    LED.play
+if defined?(Rails::Server)
+  config.after_initialize do
+    unicorn_thread =
+      Thread.new do
+        LED.play
+      end
+    unicorn_thread[:name] = 'MIDIListener'
   end
-unicorn_thread[:name] = 'MIDIListener'
+end
